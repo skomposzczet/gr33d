d520b6de88c19a70db9da74d824de7804a578372
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2021-12-29 16:18:36 +0100

    some documentation for color and board class

--- include/board.hpp
+++ include/board.hpp
@@ -8,17 +8,22 @@
 
 #include "player.hpp"
 
+/// Class handling colors in kindof easy way
 class Colorizer
 {
 public:
     Colorizer();
 
+    /// Sets color in @p window
     void set_color(WINDOW * window, int color);
+
+    /// Resets last added color in @p window
     void undo_color(WINDOW * window) {wattroff(window, COLOR_PAIR(last));}
 
     enum colors{bw, red, green, yellow, blue, magenta, cyan};
 
 private:
+    /// Keeps index of last used color
     int last;
 
 };
@@ -46,15 +51,21 @@ private:
     /// Current player
     Player player;
 
+    /// For colors
     Colorizer colorizer;
 
     /// @return reference to element on board with coordinates held by @p p
     int & at(const Point p) {return board.at(p.y).at(p.x);}
     int & at(const int x, const int y) { return at(Point(x,y));}
 
+    /// Prints symbol from board in proper way
     void print(const int x, const int y);
 
+    /// @returns random int from @p min : @p max , default is 1:9
     static int rand(const int max = 9, const int min = 1) {return ::rand()%(max-min) + min;}
 };
+
+/// ncurses wmove but handles Point class
+int wmove(WINDOW * win, Point xy);
 
 #endif
